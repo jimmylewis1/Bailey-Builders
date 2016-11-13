4a22d711f5c25f9ef0b78af7b5ed573983dc039c
Jimmy Lewis <jimmy@getweave.com>
2016-11-13 00:53:16 -0700

    added mobile nav menu close option.

--- scripts/js.js
+++ scripts/js.js
@@ -1,3 +1,11 @@
-$( ".mobile-nav" ).click(function() {
+$(".mobile-nav").click(function() {
   $(".mobile-nav-menu").css("display", "flex");
+  $(".mobile-nav").css("display", "none");
+  $(".mobile-nav-close").css("display", "block");
+});
+
+$(".mobile-nav-close").click(function() {
+  $(".mobile-nav-menu").css("display", "none");
+  $(".mobile-nav-close").css("display", "none");
+  $(".mobile-nav").css("display", "flex");
 });
