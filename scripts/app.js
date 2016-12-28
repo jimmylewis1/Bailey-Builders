6c3733cdd032253c520c3088e17e81d0b601c423
Jimmy Lewis <jimmy@getweave.com>
2016-12-28 15:20:25 -0700

    mobile fixed and working. needs styling.

--- scripts/app.js
+++ scripts/app.js
@@ -21,6 +21,16 @@ app.config(['$stateProvider', '$urlRouterProvider',
         url: '/contact',
         templateUrl: 'contact.html'
         //controller: 'servicesCtrl'
+      })
+      .state('commercial', {
+        url: '/commercial',
+        templateUrl: 'commercial.html'
+        //controller: 'servicesCtrl'
+      })
+      .state('residential', {
+        url: '/residential',
+        templateUrl: 'residential.html'
+        //controller: 'servicesCtrl'`
       });
   }
 ]);
--- scripts/js.js
+++ scripts/js.js
@@ -1,13 +1,3 @@
-//Desktop
-var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('google-map'), {
-    center: {lat: 40.397657, lng: 111.879861},
-    zoom: 18
-  });
-}
-
-
 //Mobile
 $(".mobile-nav").click(function() {
   $(".mobile-nav-menu").css("display", "flex");
@@ -21,44 +11,3 @@ $(".mobile-nav-close").click(function() {
   $(".mobile-nav").css("display", "flex");
 });
 
-//TODO
-//nav menu disappears on scroll.
-//nav menu disappears when touched outside of menu.
-
-//nav menu option blue for page (desktop)
-$(".facebook").mouseenter(
-  function() {
-    $(this).css("display", "none");
-    $(".facebook-blue").css("display", "flex");
-  }
-);
-$(".facebook-blue").mouseleave(
-  function() {
-    $(this).css("display", "none");
-    $(".facebook").css("display", "flex");
-}
-);
-$(".instagram").mouseenter(
-  function() {
-    $(this).css("display", "none");
-    $(".instagram-blue").css("display", "flex");
-  }
-);
-$(".instagram-blue").mouseleave(
-  function() {
-    $(this).css("display", "none");
-    $(".instagram").css("display", "flex");
-  }
-);
-$(".google-plus").mouseenter(
-  function() {
-    $(this).css("display", "none");
-    $(".google-blue").css("display", "flex");
-  }
-);
-$(".google-blue").mouseleave(
-  function() {
-    $(this).css("display", "none");
-    $(".google-plus").css("display", "flex");
-  }
-);
