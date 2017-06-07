e69316fc245366ee28e5525653ad0381308c325d
Jimmy Lewis <jimmy@getweave.com>
2017-06-06 21:40:12 -0600

    media queries galore.

--- scripts/mainCtrl.js
+++ scripts/mainCtrl.js
@@ -1,5 +1,7 @@
 var app = angular.module('bailey');
 
-  app.controller('mainCtrl', ['$scope', function($scope){
+  app.controller('mainCtrl', ['$scope', '$location',
+    function($scope, $location){
     $scope.test = "This is the home page.";
+    $scope.currentPath = $location.path();
   }]);
